f5c5db0f7bc6f560e3e1eb1ca3053e8262527518
qiansheng <458049863@qq.com>
2023-09-19 15:12:33 +0800

    接口返回实体类修改

--- eladmin-system/src/main/java/me/zhengjie/modules/system/rest/AppApiController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/rest/AppApiController.java
@@ -47,25 +47,25 @@ public class AppApiController {
 
     @ApiOperation("查询所有部门")
     @GetMapping("/getDeptList")
-    public ResponseEntity<Page<Dept>> getDeptList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
-                                                  @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber){
+    public ResultEntity getDeptList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
+                                    @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber){
         Page page=new Page(pageNumber,pageSize);
         QueryWrapper<Dept> queryWrapper=new QueryWrapper<>();
         Page pageList=deptService.page(page,queryWrapper);
-        return ResponseEntity.ok(pageList);
+        return ResultEntity.success(pageList.getRecords());
     }
     @ApiOperation("查询所有角色")
     @GetMapping("/getUserList")
-    public ResponseEntity<Page<User>> getUserList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
-                                                  @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber){
+    public ResultEntity getUserList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
+                                    @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber){
         Page page=new Page(pageNumber,pageSize);
         QueryWrapper<User> queryWrapper=new QueryWrapper<>();
         Page pageList=userService.page(page,queryWrapper);
-        return ResponseEntity.ok(pageList);
+        return ResultEntity.success(pageList.getRecords());
     }
     @ApiOperation("查询所有藏品用户角色")
     @GetMapping("/getCellUserList")
-    public ResponseEntity<Page<CellUser>> getCellUserList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
+    public ResultEntity getCellUserList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
                                                           @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber,
                                                           String CellUserPhone){
         Page page=new Page(pageNumber,pageSize);
@@ -74,23 +74,23 @@ public class AppApiController {
             queryWrapper.lambda().eq(CellUser::getPhone,CellUserPhone);
         }
         Page pageList=cellUserService.page(page,queryWrapper);
-        return ResponseEntity.ok(pageList);
+        return ResultEntity.success(pageList.getRecords());
     }
 
     @ApiOperation("查询所有藏品信息接口")
     @GetMapping("/cellInfoList")
-    public ResponseEntity<Page<CellInfo>> cellInfoList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
+    public ResultEntity cellInfoList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
                                                       @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber){
         Page<CellInfo> page=new Page<>(pageNumber,pageSize);
         QueryWrapper queryWrapper=new QueryWrapper();
         Page<CellInfo> pageList=cellInfoService.page(page,queryWrapper);
         ResponseEntity.status(200);
-        return ResponseEntity.ok(pageList);
+        return ResultEntity.success(pageList.getRecords());
     }
 
     @ApiOperation("藏品用户地址列表")
     @GetMapping("/collecAddressInfoByUserId")
-    public ResponseEntity<Page<CellecAddress>> cellAddressList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
+    public ResultEntity cellAddressList(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
                                                                @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber,
                                                                @RequestParam String userId){
 
@@ -98,40 +98,40 @@ public class AppApiController {
         queryWrapper.lambda().eq(CellecAddress::getUserId,userId);
         Page<CellecAddress> page=new Page<>(pageNumber,pageSize);
         Page<CellecAddress> pageList=cellecAddressService.page(page,queryWrapper);
-        return ResponseEntity.ok(pageList);
+        return ResultEntity.success(pageList.getRecords());
     }
     @ApiOperation("修改用户地址列表")
     @PostMapping("/updateAddress")
-    public ResponseEntity updateAddress(@RequestBody CellecAddress cellecAddress){
+    public ResultEntity updateAddress(@RequestBody CellecAddress cellecAddress){
         cellecAddress.setUpdateTime(new Date());
         cellecAddressService.updateById(cellecAddress);
-        return ResponseEntity.ok("修改地址成功");
+        return ResultEntity.success("修改地址成功");
     }
     @ApiOperation("删除地址信息")
     @GetMapping("/deleteAddressByIds")
-    public ResponseEntity deleteById(@RequestParam String ids){
+    public ResultEntity deleteById(@RequestParam String ids){
         cellecAddressService.removeBatchByIds(Arrays.asList(ids.split(",")));
-        return ResponseEntity.ok("删除成功");
+        return ResultEntity.success("删除成功");
     }
     @Autowired
     private CellecBankInfoService cellecBankInfoService;
     @ApiOperation("用户账户信息")
     @GetMapping("/cellecBankInfoByUserId")
-    public ResponseEntity<Page<CellecBankInfo>> cellecBankInfoByUserId(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
+    public ResultEntity cellecBankInfoByUserId(@RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
                                                                        @RequestParam(value = "pageNumber",defaultValue = "1") Integer pageNumber,
                                                                        @RequestParam String userId){
         QueryWrapper<CellecBankInfo> queryWrapper=new QueryWrapper<>();
         queryWrapper.lambda().eq(CellecBankInfo::getUserId,userId);
         Page<CellecBankInfo> page=new Page<>(pageNumber,pageSize);
         Page<CellecBankInfo> pageList=cellecBankInfoService.page(page,queryWrapper);
-        return ResponseEntity.ok(pageList);
+        return ResultEntity.success(pageList.getRecords());
     }
     @ApiOperation("修改用户账户信息")
     @PostMapping("/updateCellecBankInfo")
-    public  ResponseEntity updateCellecBankInfo(@RequestBody CellecBankInfo cellecBankInfo){
+    public  ResultEntity updateCellecBankInfo(@RequestBody CellecBankInfo cellecBankInfo){
         cellecBankInfo.setUpdateTime(new Date());
         cellecBankInfoService.updateById(cellecBankInfo);
-        return ResponseEntity.ok("修改成功");
+        return ResultEntity.success("修改成功");
     }
 
     @Autowired
