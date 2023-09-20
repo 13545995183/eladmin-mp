3b1d10b2ecb6860554573fae47dde59407d38dd5
qiansheng <458049863@qq.com>
2023-09-20 10:16:16 +0800

    添加mapper文件

--- eladmin-system/src/main/java/me/zhengjie/modules/system/mapper/CellecHaveInfoMapper.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/mapper/CellecHaveInfoMapper.java
@@ -2,10 +2,13 @@ package me.zhengjie.modules.system.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import me.zhengjie.modules.system.domain.CellecHaveInfo;
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * qiansheng
  * 2023-9-20
  *
  * */
+@Mapper
 public interface CellecHaveInfoMapper extends BaseMapper<CellecHaveInfo> {
 }
